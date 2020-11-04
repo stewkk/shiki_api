63e4725720a6b092982f1cedc44d28e0de87a666
Alexandr Starovoytov <saniy200311@gmail.com>
2020-11-04 21:24:05 +0300

    b

--- src/shiki_api.cpp
+++ src/shiki_api.cpp
@@ -11,6 +11,7 @@ Shiki::~Shiki() {
 
 std::string Shiki::api_users_id_anime_rates(int page, int limit, std::string status, bool censored) {
     std::cout << "get anime rated" << std::endl; 
+    return "test";
 }
 
 
