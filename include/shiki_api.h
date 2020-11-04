8b940201a92384967ab13523d8c4ceb13c78653a
Alexandr Starovoytov <saniy200311@gmail.com>
2020-11-04 22:31:22 +0300

    impl get anime list

--- include/shiki_api.h
+++ include/shiki_api.h
@@ -5,8 +5,12 @@
 
 class Shiki {
 public:
-    Shiki();
+    Shiki(std::string user);
     ~Shiki();
     std::string api_users_id_anime_rates(int page, int limit, std::string status, bool censored);
+private:
+    CURL* curl;
+    std::string shiki_api_domain = "https://shikimori.one/";
+    std::string shiki_api_user;
 };
 
--- src/shiki_api.cpp
+++ src/shiki_api.cpp
@@ -1,17 +1,23 @@
 
 #include "shiki_api.h"
 
-Shiki::Shiki() {
-    std::cout << "shiki_api init" << std::endl;
+Shiki::Shiki(std::string user) {
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    shiki_api_user = user;
 }
 
 Shiki::~Shiki() {
-
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
 }
 
 std::string Shiki::api_users_id_anime_rates(int page, int limit, std::string status, bool censored) {
-    std::cout << "get anime rated" << std::endl; 
-    return "test";
+    std::string ans;
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ans);
+    curl_easy_setopt(curl, CURLOPT_URL, shiki_api_domain + "api/users/" + shiki_api_user + "/anime_rates" + 
+            "?page=" + std::to_string(page) + "&limit=" + std::to_string(limit) + "&status=" + status + 
+            "&censored=" + (censored ? "true" : "false"));
+    return ans;
 }
-
 
