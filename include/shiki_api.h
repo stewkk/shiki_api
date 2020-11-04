f26fa79b63c19b66f7d072343915781ed02ec965
Alexandr Starovoytov <saniy200311@gmail.com>
2020-11-04 23:57:26 +0300

    added params

--- include/shiki_api.h
+++ include/shiki_api.h
@@ -7,7 +7,8 @@ class Shiki {
 public:
     Shiki(std::string user);
     ~Shiki();
-    std::string api_users_id_anime_rates(int page, int limit, std::string status, bool censored);
+    std::string api_users_id_anime_rates(std::string status = "watching", int limit = 5000,
+            int page = 1, bool censored = false);
 private:
     CURL* curl;
     std::string shiki_api_domain = "https://shikimori.one/";
